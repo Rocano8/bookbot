f8080583db416c68cb3747f52e3b6e4296c4b759
rocano <rogelioocano@gmail.com>
2025-06-25 19:08:50 -0700

    A: Bookbot code so far

---
+++ main.py
@@ -0,0 +1,30 @@
+from stats import get_num_words
+from stats import count_characters_loop
+from stats import sort_and_filter_char_list
+
+def get_book_text(path_to_file):
+    with open(path_to_file, 'r') as file:
+        return file.read()
+
+
+
+def main():
+    path_to_file = '/home/warseason/workspace/github.com/Rocano8/bookbot/books/frankenstein.txt'
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_to_file}...")
+    book_text = get_book_text(path_to_file)
+    num_words = get_num_words(book_text)
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    
+    character_counts = count_characters_loop(book_text)
+    char_list = [{"char": char, "num": count} for char, count in character_counts.items()]
+    sorted_alpha_list = sort_and_filter_char_list(char_list)
+    for item in sorted_alpha_list:
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
+
+   
+main()
